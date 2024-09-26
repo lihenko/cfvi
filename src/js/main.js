3d694f414c1765805b88f804580b55604a18d6c7
lihenko <obbdpu@gmail.com>
2024-09-26 12:47:37 +0300

    CFVI

--- src/js/main.js
+++ src/js/main.js
@@ -73,3 +73,156 @@ jQuery('.testimonials-slider').slick({
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
+
+var decimal_places = 0;
+var decimal_factor = decimal_places === 0 ? 1 : decimal_places * 10;
+
+  
+jQuery('#countries').viewportChecker( {
+  offset: 100, 
+  repeat: false,
+  callbackFunction: function(elem, action){
+    elem.animateNumber(
+      {
+        number: 30 * decimal_factor,
+        numberStep: function(now, tween) {
+          var floored_number = Math.floor(now) / decimal_factor,
+              target = jQuery(tween.elem);
+          if (decimal_places > 0) {
+            floored_number = floored_number.toFixed(decimal_places);
+          }
+  
+          target.text(floored_number + '+');
+        }
+      },
+      5000
+    )
+  }, 
+});
+
+jQuery('#units').viewportChecker( {
+  offset: 100, 
+  repeat: false,
+  callbackFunction: function(elem, action){
+    elem.animateNumber(
+      {
+        number: 2 * decimal_factor,
+        numberStep: function(now, tween) {
+          var floored_number = Math.floor(now) / decimal_factor,
+              target = jQuery(tween.elem);
+          if (decimal_places > 0) {
+            floored_number = floored_number.toFixed(decimal_places);
+          }
+  
+          target.text(floored_number + 'M+');
+        }
+      },
+      5000
+    )
+  }, 
+});
+
+jQuery('#years').viewportChecker( {
+  offset: 100, 
+  repeat: false,
+  callbackFunction: function(elem, action){
+    elem.animateNumber(
+      {
+        number: 50 * decimal_factor,
+        numberStep: function(now, tween) {
+          var floored_number = Math.floor(now) / decimal_factor,
+              target = jQuery(tween.elem);
+          if (decimal_places > 0) {
+            floored_number = floored_number.toFixed(decimal_places);
+          }
+  
+          target.text(floored_number + '+');
+        }
+      },
+      5000
+    )
+  }, 
+});
+
+jQuery('#history').viewportChecker( {
+  offset: 100, 
+  repeat: false,
+  callbackFunction: function(elem, action){
+    elem.animateNumber(
+      {
+        number: 20 * decimal_factor,
+        numberStep: function(now, tween) {
+          var floored_number = Math.floor(now) / decimal_factor,
+              target = jQuery(tween.elem);
+          if (decimal_places > 0) {
+            floored_number = floored_number.toFixed(decimal_places);
+          }
+  
+          target.text(floored_number + '+');
+        }
+      },
+      5000
+    )
+  }, 
+});
+
+
+jQuery('.key-industrials-slider')
+.on('init', (event, slick, currentSlide, nextSlide) => {
+  var active_slides = document.querySelectorAll("#key-industrials .slick-active");
+  var count = active_slides.length;
+  active_slides[0].classList.add('first-slide');
+  active_slides[count-1].classList.add('last-slide');
+})
+.slick({
+  infinite: true,
+  dots:false,
+  arrows:true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  centerMode:true,
+  centerPadding:'12px',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: jQuery('.key-industrials-prev-button'),
+  prevArrow: jQuery('.key-industrials-next-button'),
+  responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3,
+        arrows: true,
+        dots:false,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+        dots:true,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots:true,
+      }
+    }
+  ]
+})
+.on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+  
+  
+})
+.on('afterChange', (event, slick, currentSlide, nextSlide) => {
+  var allslides = jQuery("#key-industrials .key-industrials-slider-item");
+  allslides.removeClass('first-slide');
+  allslides.removeClass('last-slide');
+  var active_slides = document.querySelectorAll("#key-industrials .slick-active");
+  var count = active_slides.length;
+  active_slides[0].classList.add('first-slide');
+  active_slides[count-1].classList.add('last-slide');
+});
